daeb32050ebe20a9e41213c03f655e365cce2996
evgenii shiltsev <e.shiltsev@gmail.com>
2022-11-15 12:53:26 +0200

    Initial Commit

---
+++ cvProject/cvProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cvProjectApp.swift
+//  cvProject
+//
+//  Created by Evgenii Shiltsev on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct cvProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
